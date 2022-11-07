5a2c1376837d3d5d73c7e550e167e11c5d998b97
Shashinoor <109329306+Shashinoor13@users.noreply.github.com>
2022-11-07 22:13:55 +0545

    Linked

---
+++ Test/global.cpp
@@ -0,0 +1,8 @@
+#include"variables.h"
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<number;
+    return 0;
+}
---
+++ Test/variables.h
@@ -0,0 +1,6 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+ int number=75;
+
+#endif
